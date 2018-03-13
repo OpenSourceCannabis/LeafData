0f1dd4f485994b8d2de3dba3eba991453a74af44
Emanuele Tozzato <etozzato@gmail.com>
2018-03-13 17:53:01 +0000

    inventory filter were broken

--- lib/LeafData/client.rb
+++ lib/LeafData/client.rb
@@ -25,10 +25,14 @@ module LeafData
     end
 
     #
-    def has_results?(filters = {})
-      raise LeafData::Errors::MissingParameter.new('You must pass `batch_id` or `inventory_id` to the `has_results?(filters = {})` method') unless (filters.key?(:batch_id) || filters.key?(:inventory_id))
-      get_inventory({f_batch_id: filters[:batch_id], f_global_id: filters[:inventory_id]})
+    def has_results?(opts = {})
+      raise LeafData::Errors::MissingParameter.new('You must pass `batch_id` or `inventory_id` to the `has_results?(filters = {})` method') unless (opts.key?(:batch_id) || opts.key?(:inventory_id))
+      filters = {}
+      filters[:f_batch_id] = opts[:batch_id] unless opts[:batch_id].blank?
+      filters[:f_global_id] = opts[:inventory_id] unless opts[:inventory_id].blank?
+      get_inventory(filters)
       raise LeafData::Errors::NotFound.new("Sample not found") unless response['data'].any?
+      self.lab_result_id = nil
       self.lab_result_id = response['data'].first['global_lab_result_id']
       !lab_result_id.nil?
     end
