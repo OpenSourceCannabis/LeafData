f553a58d474eef623bd0ee020ea544ac47d9e95a
Emanuele Tozzato <etozzato@gmail.com>
2019-05-30 22:59:48 +0100

    opening up the full response scan

--- lib/LeafData/client.rb
+++ lib/LeafData/client.rb
@@ -25,9 +25,10 @@ module LeafData
 
       get_inventory(f_batch_id: barcode)
       return response['data'].first if response['total'] > 0
-      
+
       get_inventory
-      response['data'].find{|el| el['global_original_id'] == barcode }
+      self.response['data'] = [response['data'].find{|el| el['global_original_id'] == barcode }]
+      response['data'].first
     end
 
     def retrieve_licensee(barcode)
